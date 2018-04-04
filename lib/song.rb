0c31accf04611148f510d6bf5a55d5952341d8d3
Shea Kusiak <shkusiak@gmail.com>
2018-04-04 00:48:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,7 @@ class Song
   def self.new_from_filename(file_name)
     file_name = file_name.split(" - ")
     artist_name = file_name[0]
-    song_name = file_name[1].gsub(".mp3", "")
+    song_name = file_name[1]#.gsub(".mp3", "")
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
